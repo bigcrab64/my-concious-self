d612a172f6e689fb782f599a06b6ec1fd2996e74
Julian De La Torre <jujudlt3@gmail.com>
2022-09-26 10:16:45 -0700

    Initial Commit

---
+++ my concious self/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my concious self
+//
+//  Created by De La Torre, Julian - Student on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ my concious selfUITests/my_concious_selfUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  my_concious_selfUITestsLaunchTests.swift
+//  my concious selfUITests
+//
+//  Created by De La Torre, Julian - Student on 9/26/22.
+//
+
+import XCTest
+
+final class my_concious_selfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
